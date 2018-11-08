de23cec351f348cd445baf886c5443bcb86626fa
Marion Koo sin lin <marionksl@gmail.com>
2018-11-08 14:02:04 +0100

    Updates

---
+++ precache-manifest.234278608091c8e5868aa913859a234a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f618fc65674e32f10d51",
+    "url": "/simpson-fetch/static/js/runtime~main.f618fc65.js"
+  },
+  {
+    "revision": "a9fb24cbb9f10ba030bb",
+    "url": "/simpson-fetch/static/js/main.a9fb24cb.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/simpson-fetch/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "a9fb24cbb9f10ba030bb",
+    "url": "/simpson-fetch/static/css/main.f0c24e7c.chunk.css"
+  },
+  {
+    "revision": "e55e7776bae7d1b17d4a3dd8954089c1",
+    "url": "/simpson-fetch/index.html"
+  }
+];
